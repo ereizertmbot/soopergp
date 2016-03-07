fdd1af342866407a0abbcb227ab50e4a8bcab150
ereizertmbot <mohamadsabori55@gmail.com>
2016-03-07 21:12:35 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {95822747},
     admin_users = {},
     disabled_channels = {}
   }
